71a17bdc16b8e171eb16db1aa015271e47d63c1e
Ryo <wh404@outlook.com>
2021-10-03 00:35:13 +0100

    Added new grid selector

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function addRandomColor() {
 }
 
 function makeNewGrid() {
-    let size = Number(window.prompt('Please enter the size of new gird: '));
+    let size = Number(newGridSelector.value);
     let cellSize = 800/size;
     // Remove old cells
     while (container.lastElementChild) {
@@ -35,6 +35,7 @@ function makeNewGrid() {
         let cell = document.createElement('div');
         cell.classList.add('cell');
         cell.style.backgroundColor = 'rgb(255,255,255)';
+        cell.dataset.darken = 0;
         container.appendChild(cell);
     }
     // Add event listeners to new cells
@@ -100,30 +101,18 @@ function addShade() {
     let r = Number(color[0]);
     let g = Number(color[1]);
     let b = Number(color[2]);
-    console.log('r:'+ r);
-    console.log('g:'+ g);
-    console.log('b:'+ b);
 
     let darkenStep = Number(this.dataset.darken);
-    console.log('darkenstep:'+darkenStep);
     if (darkenStep === 10) {
         return;
     }
-    console.log('r/(10-darkenStep: '+Math.ceil(r/(10-darkenStep)));
-    console.log('g/(10-darkenStep: '+Math.ceil(g/(10-darkenStep)));
-    console.log('b/(10-darkenStep: '+Math.ceil(b/(10-darkenStep)));
-
     r = r - Math.ceil(r / (10 - darkenStep));
     r = (r < 0) ? 0 : r;
     g = g - Math.ceil(g / (10 - darkenStep));
     g = (g < 0) ? 0: g;
     b = b - Math.ceil(b / (10 - darkenStep));
     b = (b < 0) ? 0 : b;
-    console.log('new r:'+r)
-    console.log('new g:'+g)
-    console.log('new b:'+b)
-
-    
+  
     darkenStep++;
     this.dataset.darken = darkenStep;
     this.style.backgroundColor = `rgb(${r},${g},${b})`;  
@@ -146,8 +135,8 @@ let container = document.querySelector('.container');
 initialzeGrid();
 switchDefaultColorMode();
 
-let newGridBtn = document.querySelector('#newGridBtn');
-newGridBtn.addEventListener('click', makeNewGrid);
+
+let colorPicker = document.querySelector('#colorPicker');
 
 let colorBtn = document.querySelector('#colorBtn');
 colorBtn.addEventListener('click', switchDefaultColorMode);
@@ -155,13 +144,20 @@ colorBtn.addEventListener('click', switchDefaultColorMode);
 let rainbowBtn = document.querySelector('#rainbowBtn');
 rainbowBtn.addEventListener('click', switchRainbowColorMode);
 
-let eraserBtn = document.querySelector('#eraserBtn');
-eraserBtn.addEventListener('click', switchEraserMode);
-
-let colorPicker = document.querySelector('#colorPicker');
-
 let darkenBtn = document.querySelector('#darkenBtn');
 darkenBtn.addEventListener('click', switchShadeMode);
 
+let eraserBtn = document.querySelector('#eraserBtn');
+eraserBtn.addEventListener('click', switchEraserMode);
+
 let clearBtn = document.querySelector('#clearBtn');
 clearBtn.addEventListener('click', clearGrid);
+
+let newGridSelector = document.querySelector('#newGridSelector');
+let newGridNumber = document.querySelector('#newGridNumber');
+newGridSelector.addEventListener('mousemove', function() {
+    newGridNumber.textContent = newGridSelector.value + ' x ' + newGridSelector.value;
+})
+
+let newGridBtn = document.querySelector('#newGridBtn');
+newGridBtn.addEventListener('click', makeNewGrid);
